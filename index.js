99ec58e300972de3068ab7adf9689af76b82d279
tiago-ds <42779343+tiago-ds@users.noreply.github.com>
2020-06-25 22:09:41 -0300

    !cavalo e !chaos

--- index.js
+++ index.js
@@ -1,275 +1,164 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var isReady = true;
 
-const config = require("./config.json");
-const { clientId, clientSecret } = require("./config2.json");
+const config = require('./config.json');
+const links = require('./links.json');
+const ytdl = require('ytdl-core');
 
-var SpotifyWebApi = require("spotify-web-api-node");
-
-const links = require("./links.json");
-const ytdl = require("ytdl-core");
-const ytsr = require("ytsr");
-const queue = [];
-const fetch = require("node-fetch");
-fs = require("fs");
 
 client.login(config.token);
 
-client.on("ready", () => {
+client.on('ready', () =>{
     console.log("logged");
-    client.user.setActivity("pedra na casa de Manu");
+    client.user.setActivity('pedra na casa de Manu');
 });
 
-client.on("warn", console.error);
+client.on('warn', console.error);
 
-client.on("disconnect", () => console.log("Just disconnected!"));
+client.on('disconnect', () => console.log('Just disconnected!'));
 
-client.on("message", async message => {
-    // Music queue
-    const serverQueue = queue;
-
+client.on('message', async message =>{
     // In case its a bot's message
-    if (message.author.bot) return;
-    //In case the message was sent in the wrong channel
-    channelId = message.channel.id;
-    if (
-        channelId != 680967084879904778 &&
-        channelId != 680976473926270991 &&
-        channelId != 681203771036270725 &&
-        message.content.charAt(0).match("[-;!]")
-    ) {
-        message.author.send("Este não é o canal apropriado para comandos de bots.");
+    if(message.author.bot) return;
+    // In case the message was sent in the wrong channel
+    channel = message.channel;
+    if(channel.id != 680967084879904778 && channel.id != 680976473926270991 && message.content.charAt(0).match('[-;]')){
+        message.author.send('Este não é o canal apropriado para comandos de bots.');
         message.delete();
     }
     // In case its not a prefix starting message
-    if (message.content.indexOf(config.prefix) !== 0) return;
-    //divide a mensagem em um array considerando os espaços.
-    const args = message.content.toLowerCase().split(" ");
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    // Splits the message content in a array
+    const args = message.content.toLowerCase().split(' ');
     //console.log(args);
-    if (message.content.startsWith(config.prefix)) {
-        const m = args[0].replace(config.prefix, "");
-        //message.channel.send(args);
-        switch (m) {
-            case "play":
-                execute(message, serverQueue);
+    if(message.content.startsWith(config.prefix)){
+        const m = args[0].replace(config.prefix, '');
+        switch(m){
+            case('debug'):
+                message.channel.send('Ok.');
                 break;
-            case "debug":
-
-                message.channel.send("Ok.");
+            case('disconnect'):
+                disconnect(message);
                 break;
-            case "queue":
-                fila="";
-                serverQueue.forEach(element => {
-                    fila += element.song.title + "\n"
-                });
-                message.channel.send("```"+ fila +"```")
-            case "disconnect":
-                if (message.guild.voice.connection) {
-                    message.guild.voiceChannel.disconnect();
-                } else {
-                    message.channel.send("Eu não estou em um canal de voz :/");
-                }
+            case('dm'):
+                dm(message);
                 break;
-            case "dm":
-                message.author.send("Dale menó");
+            case('cavalo'):
+                cavalo(message);
+                break;
+            case('chaos'):
+                chaos(message);
                 break;
             default:
-                message.channel.send("Favor digitar um comando válido.");
+                message.channel.send('Favor digitar um comando válido.');
         }
     }
 });
 
-function getPlaylist(link) {
-    playlist_id = link.split("playlist/")[1].split("?")[0];
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('680975188581416998').send(member.user.username + ' agora faz parte do motel!');
+});
 
-    var spotifyApi = new SpotifyWebApi({
-        clientId: clientId,
-        clientSecret: clientSecret
-    });
-    return new Promise((resolve, reject) => {
-        spotifyApi.clientCredentialsGrant().then(
-            function (data) {
-                spotifyApi.setAccessToken(data.body["access_token"]);
-                spotifyApi.getPlaylist(playlist_id).then(
-                    function (data) {
-                        tracks = [];
-                        data.body.tracks.items.forEach(element => {
-                            artists = "";
-                            element.track.artists.forEach(artist => {
-                                artists += artist.name + ", ";
-                            });
-                            artists = artists.substring(0, artists.length - 2);
-                            //console.log(element.track.name + " - " + artists);
-                            tracks.push(element.track.name + " - " + artists);
-                        });
-                        resolve(tracks);
-                        return tracks;
-                    },
-                    function (err) {
-                        reject(err);
-                    }
-                );
-            },
-            function (err) {
-                reject(err);
-            }
-        );
-    });
+client.on('guildMemberRemove', member => {
+    member.guild.channels.get('680975188581416998').send(member.user.username + ' fechou sua diária!');
+    member.send('Bem vindo ao devaneios! :)');
+});
+
+async function cavalo(message){
+    console.log('cavalo!')
+    message.channel.send({files: ['attachments/cavalo.gif']});
+    channel = message.channel;
+    //setTimeout(() => { channel.lastMessage.delete(); }, 1000);
 }
 
-client.on("guildMemberAdd", member => {
-    member.guild.channels
-        .get("680975188581416998")
-        .send(member.user.username + " agora faz parte do motel!");
-});
+async function dm(message){
+    console.log('Sending dm to ' + message.author.username);
+    message.author.send('Dale menó');
+}
 
-client.on("guildMemberRemove", member => {
-    member.guild.channels
-        .get("680975188581416998")
-        .send(member.user.username + " fechou sua diária!");
-    member.send("Bem vindo ao devaneios! :)");
-});
-
-async function execute(message, serverQueue) {
-    const arr = message.content.replace("!play ", "");
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel) {
-        return message.channel.send(
-            "Você precisa estar conectado a um canal de voz!"
-        );
+async function disconnect(message){
+    if(message.guild.voiceConnection){
+        message.guild.voiceConnection.disconnect();
+    }else{
+        message.channel.send('Eu não estou em um canal de voz :/');
     }
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "Painho não me deu permissão pra entrar nesse canal de voz :("
-        );
+}
+//Chaos part 1. Here I just play the noisy song and call chaos part 2
+async function chaos(message){
+    if(isReady){
+        //That exists so someone can't call the bot inside the voice channel if he is in one already.
+        isReady = false;
+
+        var voiceChannel = message.member.voiceChannel;
+        voiceChannel.join().then(connection =>
+            {
+                const dispatcher = connection.playFile('./caos.mp3');
+                dispatcher.on('end', end => {
+                    //console.log('finished caos song');
+                    chaos2(message);
+                    voiceChannel.leave();
+                });
+            }).catch(err => console.log(err));
+            isReady = true;
     }
-    var connection = await voiceChannel.join();
-    if (message.content.replace("!play ", "").match("open.spotify.com")) {
-        tracks = await getPlaylist(message.content.replace("!play ", ""));
-        await tracks.forEach(async function (element) {
-            music = await search(element);
-            await console.log(element + ": " + music)
-            const info = await ytdl.getInfo(music.split("?v=")[1]);
-            const song = {
-                title: info.title,
-                url: info.video_url
-            };
+}
 
-            const queueConstruct = {
-                textChannel: message.channel,
-                voiceChannel: voiceChannel,
-                connection: null,
-                song: song,
-                volume: 5,
-                playing: false
-            };
+async function chaos2(message){
+    //Why do i have to do a try/catch just to delete a message? Dunno >:
+    try {
+        await message.delete();   
+    } catch (error) {
+        console.log(error)
+    }
+    //Gets the voice channel that will (primaly) suffer the chaos
+    voiceChannel = message.member.voiceChannel;
+    //Gets the list of all voice channels in the guild
+    const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
+    //Gets the list of all users in the voiceChannel
+    activeUsers = voiceChannel.members.filter(user => !user.bot);
+    //Gets the channel that the CHAOS started
+    msgChannel = message.channel;
+    //Finally, calls the function chaos3, that will move people around and create REAL chaos
+    chaos3(0, voiceChannel, voiceChannels, activeUsers, msgChannel);
+}
 
-            queueConstruct.connection = connection;
-
-            await serverQueue.push(queueConstruct);
-            try {
-                if (!serverQueue[0].playing) {
-                    playMusic(message.guild, serverQueue);
-                }
-            } catch (err) {
-                console.log(err)
-            }
-        });
-        
-    } else {
-        video = await search(arr);
-
-        console.log(video);
-
-        const info = await ytdl.getInfo(video.split("?v=")[1]);
-        const song = {
-            title: info.title,
-            url: info.video_url
-        };
-        if (!serverQueue) {
-            console.log("Teste");
-        } else {
-            try {
-                await message.channel.send(`${song.title} foi adicionada à fila!`);
-            } catch (err) {
-                console.log(err);
-            }
+//that's not working :<
+/* function chaos3(voiceChannel, voiceChannels, activeUsers){
+        for(x = 0; x < 5; x++){
+            randomKey = activeUsers.randomKey();
+            usuario = activeUsers.get(randomKey);
+            usuario.setVoiceChannel(voiceChannels.randomKey());
+            sleep(1000);
         }
+} */
 
-        const queueConstruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            song: song,
-            volume: 5,
-            playing: false
-        };
-
-        try {
-            queueConstruct.connection = connection;
-            serverQueue.push(queueConstruct);
-            console.log(serverQueue[0].playing);
-            if (!serverQueue[0].playing) {
-                await playMusic(message.guild, serverQueue);
-            }
-        } catch (err) {
-            console.log(err);
-            //queue.delete(message.guild.id);
-            return message.channel.send(err);
+function chaos3(counter, voiceChannel, voiceChannels, activeUsers, msgChannel){
+    // That's how I made it work. Recursively calling the function chaos with a counter that goes from 1 to 10.
+    if(counter < 2){
+        setTimeout(function() {
+            // First, incremente the counter to the next call
+            counter++;
+            // Here, goes the code to randomly switch channels
+            randomKey = activeUsers.randomKey();
+            usuario = activeUsers.get(randomKey);
+            usuario.setVoiceChannel(voiceChannels.randomKey());
+            // There's the recursivity
+            chaos3(counter, voiceChannel, voiceChannels, activeUsers, msgChannel);
+        }, 1500);
+    }else{
+        // For sure I can improve that.
+        usersArray = activeUsers.firstKey(activeUsers.array().length);
+        // The last iteration that runs ALL the active userss array, sending them to the original channel.
+        for(x = 0; x < usersArray.length; x++){
+            usuario = activeUsers.get(usersArray[x]);
+            // Console.log('Deslocando ' + usuario.username + ' para o canal ' + voiceChannel.name + '.');
+            usuario.setVoiceChannel(voiceChannel.id.toString());
         }
     }
 }
 
-function search(songName) {
-    return new Promise((resolve, reject) => {
-        ytsr.getFilters(songName, function (err, filters) {
-            if (err) {
-                reject(err);
-            }
-            filter = filters.get("Type").find(o => o.name === "Video");
-            var options = {
-                limit: 5,
-                nextpageRef: filter.ref
-            };
-            ytsr(null, options, function (err, searchResults) {
-                if (err) {
-                    reject(err);
-                }
-                video_id = searchResults.items[0].link;
-                console.log(video_id);
-                resolve(video_id);
-                return video_id;
-            });
-        });
-    });
-}
-/**
- * Play the music on a voice channel
- * @param {*} guild 
- * @param {*} serverQueue 
- */
-function playMusic(guild, serverQueue) {
-    if (serverQueue.length <= 0) {
-        serverQueue[0].textChannel.send("Morri");
-        guild.voiceChannel.disconnect();
-        return;
-    }
-    console.log(serverQueue.lenght);
-    if (serverQueue.length > 0) {
-        console.log(serverQueue[0].song.title);
-        const dispatcher = serverQueue[0].connection
-            .playStream(ytdl(serverQueue[0].song.url))
-            .on("end", () => {
-                serverQueue.shift();
-                console.log(serverQueue.length);
-                playMusic(guild, serverQueue);
-            })
-            .on("error", error => console.error(error));
-        serverQueue[0].playing = true;
-        dispatcher.setVolumeLogarithmic(serverQueue[0].volume / 5);
-        serverQueue[0].textChannel.send(
-            `Tocando agora ${serverQueue[0].song.title}`
-        );
-    }
-}
+//that shit do not work
+/* function sleep(milliseconds){
+    return new Promise((resolve) => {setTimeout(resolve, milliseconds)});
+} */

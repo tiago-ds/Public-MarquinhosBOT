d15126284e6e26315a09df4ebe59c79f4ce708b3
Tiago <tscs2@cin.ufpe.br>
2020-08-10 17:22:29 -0300

    regexfraco

--- index.js
+++ index.js
@@ -28,8 +28,9 @@ client.on('disconnect', () => console.log('Just disconnected!'));
 client.on('message', async message =>{
     // In case its a bot's message
     if(message.author.bot) return;
-    re = new RegExp(/b.*d.*a|g.*m.*/gi);
-    if(re.test(message.content)){
+    re = new RegExp(/bom dia/gi);
+
+    if(re.test(accentsTidy(message.content))){
         message.delete();
         message.channel.send(`${message.content} é o caralho.`)
         message.channel.fetchMessages({ limit: 1 }).then(messages => {
@@ -520,3 +521,20 @@ function get_bicho(numero){
         return `no ${bichos[randCeil]} ${randint}`
     return `na ${bichos[randCeil]} ${randint}`
 }
+
+accentsTidy = function(s){
+    var r=s.toLowerCase();
+    r = r.replace(new RegExp(/\s/g),"");
+    r = r.replace(new RegExp(/[àáâãäå]/g),"a");
+    r = r.replace(new RegExp(/æ/g),"ae");
+    r = r.replace(new RegExp(/ç/g),"c");
+    r = r.replace(new RegExp(/[èéêë]/g),"e");
+    r = r.replace(new RegExp(/[ìíîï]/g),"i");
+    r = r.replace(new RegExp(/ñ/g),"n");                
+    r = r.replace(new RegExp(/[òóôõö]/g),"o");
+    r = r.replace(new RegExp(/œ/g),"oe");
+    r = r.replace(new RegExp(/[ùúûü]/g),"u");
+    r = r.replace(new RegExp(/[ýÿ]/g),"y");
+    r = r.replace(new RegExp(/\W/g),"");
+    return r;
+};

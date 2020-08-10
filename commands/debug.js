fe0462eaaaecd495cf3b4a562176fa15fc6c3ae1
Guilherme Afonso <gasp@cin.ufpe.br>
2020-08-10 00:50:32 -0300

    Adiciona winston ao debug

--- commands/debug.js
+++ commands/debug.js
@@ -1,7 +1,8 @@
+const logging = require("./utils/debugger");
 module.exports = {
     name: "debug",
     description: "Incorporo o Rammus",
     execute(message, args) {
-        message.channel.send("Ok.");
+        message.channel.send("Ok?");
     },
 };
---
+++ utils/logger.js
@@ -0,0 +1,52 @@
+const winston = require("winston");
+const colorizer = winston.format.colorize();
+
+const config = {
+    levels: {
+        error: 0,
+        debug: 1,
+        warn: 2,
+        data: 3,
+        info: 4,
+        verbose: 5,
+        silly: 6,
+        custom: 7,
+    },
+    colors: {
+        error: "red",
+        debug: "blue",
+        warn: "yellow",
+        data: "grey",
+        info: "green",
+        verbose: "cyan",
+        silly: "magenta",
+        custom: "yellow",
+    },
+};
+
+const logger = winston.createLogger({
+    levels: config.levels,
+    format: winston.format.combine(
+        winston.format.timestamp({ format: "DD/MM/YY HH:mm:ss" }),
+        winston.format.printf(
+            (info) =>
+                colorizer.colorize(
+                    info.level,
+                    `[${info.timestamp}] ${info.level.replace(/\b\w/g, (l) => {
+                        return l.toUpperCase();
+                    })}`
+                ) + `: ${info.message}`
+        )
+    ),
+    transports: [
+        new winston.transports.Console({ timestamp: true }),
+        new winston.transports.File({
+            filename: serverLogFolder,
+            timestamp: true,
+            level: "info",
+        }),
+    ],
+    level: "custom",
+});
+
+module.exports = {logger};

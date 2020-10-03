96df607528f64bac264c84747e783ee75a5a0342
Tiago <tscs2@cin.ufpe.br>
2020-10-02 21:32:34 -0300

    Avança no !diga

--- commands/diga.js
+++ commands/diga.js
@@ -1,26 +1,41 @@
-const { randomInt } = require('crypto');
 const fetch = require('node-fetch');
+require('dotenv').config();
+
 const tmdb_key = '4d607ca745fa466f945f58c6a40e832'
 
 
 module.exports = {
     name: "diga",
     description: "Eu te recomendo algo :)",
-    execute(message, args) {
-        type = args[1];
-        if(args[1].match('filme')){
-            let URL_TO_FETCH = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb_key}d&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=1`;
+    async execute(message, args) {
+        message.delete();
+        qtd = args[0];
+        pedido = args[1];
+        if(pedido.match('filme')){
+            let URL_TO_FETCH = `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_API_KEY}`;
             fetch(URL_TO_FETCH, { 
                 method: 'get'
             }).then(
                 function(response){
                     response.json().then(function(data){
-                        console.log(data);
+                        filme = data.results.sample();
+                        console.log(filme);
                     });
                 }
             ).catch(function(err){
                 console.log(err);
             });
+        }else if(pedido.match('serie')){
+
         }
     },
 };
+
+async function diga(qtd){
+    let URL_TO_FETCH
+}
+
+
+Array.prototype.sample = function(){
+    return this[Math.floor(Math.random()*this.length)];
+}

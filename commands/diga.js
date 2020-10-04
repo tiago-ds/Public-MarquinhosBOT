fc050bcf8d33aba8689174c3e307b5a7ab3dbce8
Tiago <tscs2@cin.ufpe.br>
2020-10-03 21:27:34 -0300

    Finaliza (por enquanto) !diga

--- commands/diga.js
+++ commands/diga.js
@@ -9,67 +9,83 @@ module.exports = {
     async execute(message, args) {
         //message.delete();
         let obj;
-        if(args[0] > 3){
+        if(isNaN(args[0]) || (args[0] > 3 || (args[0] <= 0))){
             message.channel.send('Danousse, irmão. Até 3 eu até dou, mas aí tu avacalhou.');
         }else{
             if(args[1].match('filme')){
-                obj = await digaFilme();
+                obj = await digaFilme(Math.floor(args[0]));
             }else if(args[1].match('serie')){
-                obj = await digaSerie();
+                obj = await digaSerie(Math.floor(args[0]));
             }else{
                 message.channel.send(`Tu quer que eu te indique ${args[1]} mesmo?`
                 + ` Eu só sei de filme ou série.`);
             }
         }
-        if(obj.title != ''){
-            await message.channel.send(await criarEmbed(obj))
-            .catch((err) => console.log(err));
-        }else{
-            await message.channel.send("Deu algo errado aí. Tenta de novo, eu acho.");
+        for(x = 0; x < obj.length; x++){
+            if(obj[x].title != ''){
+                await message.channel.send(await criarEmbed(obj[x]))
+                .catch((err) => console.log(err));
+            }else{
+                await message.channel.send("Deu algo errado aí. Tenta de novo, eu acho.");
+            }
         }
         //message.channel.send(`Que tal assistir ${filme.title}?\nAqui está uma descrição: ${filme.overview}\n`);
     },
 };
 
-async function digaFilme(){
+async function digaFilme(amt){
     let URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${Math.floor(Math.random() * 500)}`;
     
     let response = await (fetch(URL, { method: 'get' }));
     let dados = await response.json();
     
-    ({title, release_date, overview, poster_path } = dados.results.sample());
-    
-    let filme = {
-        title,
-        release_date,
-        overview,
-        poster_path
-    }
+    filmes = [];
 
-    return filme;
+    for(x = 0; x < amt; x++){
+        ({title, release_date, overview, poster_path } = dados.results.sample());
+    
+        let filme = {
+            title,
+            release_date,
+            overview,
+            poster_path
+        }
+        
+        filmes.push(filme);
+
+    }
+    return filmes;
 }
 
-async function digaSerie(){
+async function digaSerie(amt){
     let URL = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.TMDB_API_KEY}&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${Math.floor(Math.random() * 500)}`;
     
     let response = await (fetch(URL, { method: 'get' }));
     let dados = await response.json();
     
-    let {name : title, first_air_date : release_date, overview, poster_path } = dados.results.sample();
-    
-    let serie = {
-        title,
-        release_date,
-        overview,
-        poster_path
-    }
+    let series = [];
 
-    return serie;
+    for(x = 0; x < amt; x++){
+        let {name : title, first_air_date : release_date, overview, poster_path } = dados.results.sample();
+    
+        let serie = {
+            title,
+            release_date,
+            overview,
+            poster_path
+        }
+
+        series.push(serie);
+    }
+    return series;
 }
 
 function criarEmbed(obj){
+    let titulo = `Nome: ${obj.title}`;
+    if(obj.release_date)
+        titulo += `(${obj.release_date.split('-')[0]})`;
     let embed = new Discord.MessageEmbed()
-        .setTitle(`Nome: ${obj.title} (${obj.release_date.split('-')[0]})`)
+        .setTitle(titulo)
         .setColor("#0099ff")
         .setThumbnail(`https://image.tmdb.org/t/p/w500/${obj.poster_path}`);
     if(obj.overview != ''){
@@ -77,7 +93,7 @@ function criarEmbed(obj){
             name: "Descrição:",
             value:
                 obj.overview
-        })
+        });
     }
     return embed;
 }

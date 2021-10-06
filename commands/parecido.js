c609fa253a587206a853e594d5f254ab68480d51
di3goCS <diego.cs282@gmail.com>
2021-10-05 23:23:26 -0300

    Adiciona comando de pedido de desculpas

---
+++ commands/parecido.js
@@ -0,0 +1,9 @@
+const player = require("../utils/player");
+module.exports = {
+    name: "parecido",
+    description: "Eu queria explicar pra vocês, que não foi isso...",
+    execute(message, args) {
+        newUserChannel = message.member.voice.channel;
+        player.execute(message, "./resources/sounds/podeterparecido.mp3", newUserChannel);
+    },
+};

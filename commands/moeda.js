9111fd9a20cb8da00b88e754798b372347882865
Tiago <tscs2@cin.ufpe.br>
2021-10-28 21:10:55 -0300

    Altera o funcionamento do comando !moeda de forma a ficar mais interessante

--- commands/moeda.js
+++ commands/moeda.js
@@ -2,15 +2,16 @@ module.exports = {
     name: "moeda",
     aliases: ["coin"],
     description: "Tiro cara ou coroa numa moeda semi-viciada.",
-    execute(message, args) {
+    usage: "!moeda | !coin",
+    async execute(message) {
         // Just a function that gets a random number between 0 or 1
         randint = Math.floor(Math.random() * 2);
-        // If the random number is 1, print CARA!
+        // If the random number is 1, send a HEAD embed!
         if(randint === 1){
-            message.channel.send('CARA!');
+            message.channel.send({files: ["./resources/images/coin_head.png"]});
         }else{
-        // Just print COROA!
-            message.channel.send('COROA!');
+        // Tails!
+            message.channel.send({files: ["./resources/images/coin_tail.png"]});
         }
     },
 };
